683c9e1b30e2e9f6253c00494602e956a5ee1d08
bitte-ein-bit <code@v-o-g-t.net>
2013-08-20 22:10:13 +0200

    Initial Version

---
+++ Gruppeneditor/FormGruppeneditor.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Gruppeneditor
+{
+    partial class FormGuppeneditor
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBoxGruppe = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBoxMember = new System.Windows.Forms.GroupBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxMember = new System.Windows.Forms.ComboBox();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.listViewMember = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBoxMember.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBoxGruppe
+            // 
+            this.comboBoxGruppe.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.comboBoxGruppe.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxGruppe.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxGruppe.FormattingEnabled = true;
+            this.comboBoxGruppe.Items.AddRange(new object[] {
+            "Gruppe 1",
+            "Gruppe 2"});
+            this.comboBoxGruppe.Location = new System.Drawing.Point(144, 6);
+            this.comboBoxGruppe.Name = "comboBoxGruppe";
+            this.comboBoxGruppe.Size = new System.Drawing.Size(464, 21);
+            this.comboBoxGruppe.Sorted = true;
+            this.comboBoxGruppe.TabIndex = 2;
+            this.comboBoxGruppe.SelectedIndexChanged += new System.EventHandler(this.comboBoxGruppe_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Zu bearbeitende Gruppe:";
+            // 
+            // groupBoxMember
+            // 
+            this.groupBoxMember.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxMember.Controls.Add(this.buttonAdd);
+            this.groupBoxMember.Controls.Add(this.label2);
+            this.groupBoxMember.Controls.Add(this.comboBoxMember);
+            this.groupBoxMember.Controls.Add(this.buttonRemove);
+            this.groupBoxMember.Controls.Add(this.listViewMember);
+            this.groupBoxMember.Enabled = false;
+            this.groupBoxMember.Location = new System.Drawing.Point(15, 33);
+            this.groupBoxMember.Name = "groupBoxMember";
+            this.groupBoxMember.Size = new System.Drawing.Size(593, 436);
+            this.groupBoxMember.TabIndex = 4;
+            this.groupBoxMember.TabStop = false;
+            this.groupBoxMember.Text = "Gruppenmitglieder";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAdd.Location = new System.Drawing.Point(431, 17);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 6;
+            this.buttonAdd.Text = "hinzufügen";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Suchen";
+            // 
+            // comboBoxMember
+            // 
+            this.comboBoxMember.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxMember.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBoxMember.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.comboBoxMember.FormattingEnabled = true;
+            this.comboBoxMember.Location = new System.Drawing.Point(56, 19);
+            this.comboBoxMember.Name = "comboBoxMember";
+            this.comboBoxMember.Size = new System.Drawing.Size(369, 21);
+            this.comboBoxMember.TabIndex = 4;
+            this.comboBoxMember.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBoxMember_KeyDown);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRemove.Location = new System.Drawing.Point(512, 17);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(75, 23);
+            this.buttonRemove.TabIndex = 3;
+            this.buttonRemove.Text = "löschen";
+            this.toolTip1.SetToolTip(this.buttonRemove, "Markierte löschen");
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // listViewMember
+            // 
+            this.listViewMember.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewMember.CheckBoxes = true;
+            this.listViewMember.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listViewMember.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listViewMember.Location = new System.Drawing.Point(6, 46);
+            this.listViewMember.Name = "listViewMember";
+            this.listViewMember.ShowGroups = false;
+            this.listViewMember.Size = new System.Drawing.Size(581, 373);
+            this.listViewMember.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listViewMember.TabIndex = 2;
+            this.listViewMember.UseCompatibleStateImageBehavior = false;
+            this.listViewMember.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 195;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Email";
+            this.columnHeader2.Width = 338;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.Enabled = false;
+            this.buttonSave.Location = new System.Drawing.Point(533, 480);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 5;
+            this.buttonSave.Text = "Speichern";
+            this.toolTip1.SetToolTip(this.buttonSave, "Speichert die Änderungen an den Mitglieder dieser Gruppe");
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // FormGuppeneditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 515);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.groupBoxMember);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxGruppe);
+            this.Name = "FormGuppeneditor";
+            this.Text = "Gruppeneditor";
+            this.Load += new System.EventHandler(this.FormGroupEditor_Load);
+            this.groupBoxMember.ResumeLayout(false);
+            this.groupBoxMember.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxGruppe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBoxMember;
+        private System.Windows.Forms.ListView listViewMember;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxMember;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.ToolTip toolTip1;
+
+    }
+}
+
---
+++ Gruppeneditor/FormGruppeneditor.cs
@@ -0,0 +1,277 @@
+﻿/*
+ * Gruppeneditor
+ * 
+ * (c) Jonathan Vogt
+ * 
+ * License GPL
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices;
+
+namespace Gruppeneditor
+{
+    public partial class FormGuppeneditor : Form
+    {
+        public FormGuppeneditor()
+        {
+            InitializeComponent();
+        }
+
+        private Hashtable UserDNTable = new Hashtable();
+        private Hashtable GroupDNTable = new Hashtable();
+        private Hashtable GroupMember = new Hashtable();
+
+        private System.Security.Principal.WindowsIdentity GetCurrentUser()
+        {
+            return System.Security.Principal.WindowsIdentity.GetCurrent();
+        }
+
+        private string GetSamAccountName()
+        {
+            String tmp = GetCurrentUser().Name;
+            // TODO
+            //tmp = "TEST\\LArm";
+            return tmp.Substring(tmp.LastIndexOf('\\'));
+        }
+
+        private PrincipalContext GetPrincipalContext()
+        {
+            // TODO
+            //return new PrincipalContext(ContextType.Domain, "test.local", "TEST\\LArm", "abc123!");
+            return new PrincipalContext(ContextType.Domain);
+        }
+
+        private void FindAllUser()
+        {
+            try
+            {
+                PrincipalContext AD = GetPrincipalContext();
+                UserPrincipal u = new UserPrincipal(AD);
+                PrincipalSearcher search = new PrincipalSearcher(u);
+                comboBoxMember.AutoCompleteCustomSource.Clear();
+
+                foreach (UserPrincipal result in search.FindAll())
+                {
+                    if (result.Enabled == true && result.EmailAddress != null && result.DisplayName != null)
+                    {
+                        string tmp = String.Format("{0}, {1}", result.Surname, result.GivenName);
+                        UserDNTable.Add(tmp.ToLowerInvariant(), result.DistinguishedName);
+                        comboBoxMember.AutoCompleteCustomSource.Add(tmp);
+                    }
+                }
+                search.Dispose();
+            }
+
+            catch (Exception e)
+            {
+                showError(e);
+            }
+        }
+
+        private string FindMyDN()
+        {
+            try
+            {
+                PrincipalContext AD = GetPrincipalContext();
+                UserPrincipal u = new UserPrincipal(AD);
+                u.SamAccountName = GetSamAccountName();
+                PrincipalSearcher search = new PrincipalSearcher(u);
+
+                Principal user = search.FindOne();
+
+                search.Dispose();
+                return user.DistinguishedName;
+            }
+            catch (Exception e)
+            {
+                showError(e);
+                return null;
+            }
+        }
+
+        private void FindMyGroups()
+        {
+            try
+            {
+                DirectorySearcher search = GetDirectorySearcher();
+                string dn = FindMyDN();
+                if (dn == null) return;
+                search.Filter = "(managedBy=" + dn + ")";
+                comboBoxGruppe.Items.Clear();
+                GroupDNTable.Clear();
+                foreach (SearchResult result in search.FindAll())
+                {
+                    string name = result.Properties["name"][0].ToString();
+                    comboBoxGruppe.Items.Add(name);
+                    if (GroupDNTable.ContainsKey(name.ToLowerInvariant()))
+                    {
+                        throw new NotImplementedException();
+                    }
+                    GroupDNTable.Add(name.ToLowerInvariant(), result.Properties["distinguishedName"][0]);
+                }
+            }
+            catch (Exception e)
+            {
+                showError(e);
+            }            
+        }
+
+        private SearchResult GetUserForDN(string dn)
+        {
+            try
+            {
+                DirectorySearcher search = GetDirectorySearcher();
+                search.Filter = "(distinguishedName=" + dn + ")";
+                return search.FindOne();
+            }
+            catch (Exception e)
+            {
+                showError(e);
+                return null;
+            }
+        }
+
+        private void GetGroupMember(string groupname)
+        {
+            try
+            {
+                DirectorySearcher search = GetDirectorySearcher();
+                search.Filter = "(distinguishedName=" + GroupDNTable[groupname.ToLowerInvariant()] + ")";
+                clearMemberList();
+                SearchResult result = search.FindOne();
+                for (int i = 0; i < result.Properties["member"].Count; i++)
+                {
+                    addUserToMemberList(result.Properties["member"][i].ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                showError(e);
+            }
+        }
+
+        private DirectorySearcher GetDirectorySearcher()
+        {
+            DirectoryEntry ldapConnection = new DirectoryEntry("LDAP://test.local");
+            ldapConnection.AuthenticationType = AuthenticationTypes.Secure;
+            // TODO
+            //ldapConnection.Username = "LArm@test.local";
+            //ldapConnection.Password = "abc123!";
+            return new DirectorySearcher(ldapConnection);
+        }
+
+        private void comboBoxGruppe_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetGroupMember(comboBoxGruppe.SelectedItem.ToString());
+            groupBoxMember.Enabled = true;
+        }
+
+        private void addUserToMemberList(string distinguishedName)
+        {            
+            SearchResult user = GetUserForDN(distinguishedName);
+            if (user == null) return;
+            string displayName = user.Properties["displayName"][0].ToString();
+            if (!GroupMember.ContainsKey(displayName.ToLowerInvariant()))
+            {
+                ListViewItem lvi = new ListViewItem(displayName);
+                lvi.SubItems.Add(user.Properties["mail"][0].ToString().ToLowerInvariant());
+                listViewMember.Items.Add(lvi);
+                GroupMember.Add(displayName.ToLowerInvariant(), distinguishedName);
+            }
+        }
+
+        private void clearMemberList()
+        {
+            listViewMember.Items.Clear();
+            GroupMember.Clear();
+        }
+
+        private void removeMember(string displayName)
+        {
+            foreach (ListViewItem item in listViewMember.Items)
+            {
+                if (item.Text.Equals(displayName))
+                {
+                    item.Remove();
+                }
+            }
+            GroupMember.Remove(displayName);
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (UserDNTable.ContainsKey(comboBoxMember.Text.ToLowerInvariant()))
+            {
+                addUserToMemberList(UserDNTable[comboBoxMember.Text.ToLowerInvariant()].ToString());
+            }
+            else
+            {
+                MessageBox.Show("Benutzer nicht gefunden", "Information", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            buttonSave.Enabled = true;
+        }
+
+        private void comboBoxMember_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                buttonAdd_Click(this, null);
+            }
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in listViewMember.CheckedItems)
+            {
+                removeMember(item.Text);
+            }
+            buttonSave.Enabled = false;
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            DirectorySearcher search = GetDirectorySearcher();
+            search.Filter = "(distinguishedName=" + GroupDNTable[comboBoxGruppe.SelectedItem.ToString().ToLowerInvariant()] + ")";
+            SearchResult result = search.FindOne();
+            DirectoryEntry entry = result.GetDirectoryEntry();
+            entry.Properties["member"].Clear();
+            
+            foreach (string distinguishedName in GroupMember.Values)
+            {
+                entry.Properties["member"].Add(distinguishedName);
+            }
+            entry.CommitChanges();
+            buttonSave.Enabled = false;
+        }
+
+        private void showError(Exception e)
+        {
+            MessageBox.Show(e.Message, "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Application.Exit();
+        }
+
+        private void FormGroupEditor_Load(object sender, EventArgs e)
+        {
+            FindAllUser();
+            FindMyGroups();
+            if (GroupDNTable.Count == 0)
+            {
+                MessageBox.Show("Sie sind bei keiner Gruppe als verwaltungsberechtigt eingetragen.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+    }
+}
